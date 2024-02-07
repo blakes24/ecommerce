9bf854f0c8596813546427712787fb9720740875
blakes24 <blakes24@gmail.com>
2024-02-07 13:08:37 -0800

    Add middleware

---
+++ src/routes/verifyToken.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        res.status(403).json("Invalid token");
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("Not authorized");
+  }
+};
+
+export const verifyAndAuthorize = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Permission denied");
+    }
+  });
+};
+
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Permission denied");
+    }
+  });
+};
