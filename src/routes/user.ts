cc940df00cc43391eaac548f28cd9b2de45421d6
blakes24 <blakes24@gmail.com>
2024-02-07 13:09:17 -0800

    Add route to update user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,33 @@
+import CryptoJS from "crypto-js";
 import { Router } from "express";
+import { verifyAndAuthorize, verifyToken } from "./verifyToken.js";
+import User from "../models/User.js";
+
 const router = Router();
 
 router.get("/test", function (req, res, next) {
   return res.status(200).json({ msg: "user test" });
 });
 
-router.post("/", function (req, res, next) {
-  const { userName } = req.body;
-  console.log(userName);
-  return res.status(200).json({ msg: userName });
+router.patch("/:id", verifyAndAuthorize, async function (req, res, next) {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 export default router;
