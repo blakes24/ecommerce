59734a14b184373c71c82cc7f6b0653a8f1e3af1
blakes24 <blakes24@gmail.com>
2024-02-07 14:44:12 -0800

    Add routes to get user info

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 import CryptoJS from "crypto-js";
 import { Router } from "express";
-import { verifyAndAuthorize, verifyToken } from "./verifyToken.js";
+import { verifyAdmin, verifyAndAuthorize, verifyToken } from "./verifyToken.js";
 import User from "../models/User.js";
 
 const router = Router();
@@ -9,6 +9,28 @@ router.get("/test", function (req, res, next) {
   return res.status(200).json({ msg: "user test" });
 });
 
+// GET USER
+router.get("/:id", verifyAdmin, async function (req, res, next) {
+  try {
+    const user = (await User.findById(req.params.id)).toObject();
+    delete user.password;
+    return res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET All USERS
+router.get("/", verifyAdmin, async function (req, res, next) {
+  try {
+    const users = await User.find().select("-password");
+    return res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PATCH
 router.patch("/:id", verifyAndAuthorize, async function (req, res, next) {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
@@ -25,6 +47,16 @@ router.patch("/:id", verifyAndAuthorize, async function (req, res, next) {
       { new: true }
     );
     return res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyAndAuthorize, async function (req, res, next) {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    return res.status(200).json("User Deleted");
   } catch (err) {
     res.status(500).json(err);
   }
